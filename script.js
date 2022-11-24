076801b9b38d6507dc7965b613d372e312cf2fc9
VVilchak <83465049+VVilchak@users.noreply.github.com>
2022-11-24 13:59:47 +0200

    updated  js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    displayMessage('No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('Correct number!:)');
+    document.querySelector('.number').textContent = secretNumber;
+
+    score++;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#69db7c';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? `Too high :(` : `Too low :(`);
+      score--;
+      //score = score -1
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  displayMessage(`Start guessing...`);
+  document.querySelector('.number').textContent = `?`;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = ``;
+});
